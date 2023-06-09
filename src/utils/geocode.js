42460534765d8863b5d8051ff86917d3acb91f45
kevit-shekhar-tayde <shekhar.tayde@kevit.io>
2023-06-09 11:58:42 +0530

    current location forecast button added.

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,29 +1,48 @@
 const geocode = async (req, res) => {
-  if (!req.query.address) {
+  if (!req.query.address && !req.query.latitude && !req.query.longitude) {
     return res.status(400).json({
       error: "You must provide an address.",
     });
   }
-  console.log(req.query.address);
-  const url = `http://api.weatherstack.com/current?access_key=30088fb36eb2dda3c83bd80f4267d4f3&query=${req.query.address}`;
   let forecastData;
-  try {
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        forecastData = data;
-      });
-    console.log(forecastData);
-    if (forecastData.error) {
+  if (req.query.latitude && req.query.longitude && !req.query.address) {
+    const url = `http://api.weatherstack.com/current?access_key=30088fb36eb2dda3c83bd80f4267d4f3&query=${req.query.latitude},${req.query.longitude}`;
+    try {
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          forecastData = data;
+        });
+      if (forecastData.error) {
+        return res.status(400).json({
+          error: "Unable to find location. Try another search.",
+        });
+      }
+    } catch (err) {
       return res.status(400).json({
-        error: "Unable to find location. Try another search.",
+        error: "Unable to connect to weather service.",
       });
     }
-  } catch (err) {
-    return res.status(400).json({
-      error: "Unable to connect to geo-location service.",
-    });
+  } else {
+    const url = `http://api.weatherstack.com/current?access_key=30088fb36eb2dda3c83bd80f4267d4f3&query=${req.query.address}`;
+    try {
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          forecastData = data;
+        });
+      if (forecastData.error) {
+        return res.status(400).json({
+          error: "Unable to find location. Try another search.",
+        });
+      }
+    } catch (err) {
+      return res.status(400).json({
+        error: "Unable to connect to weather service.",
+      });
+    }
   }
+
   res.status(200).json({
     place: `${forecastData.location.name}, ${forecastData.location.region}, ${forecastData.location.country}.`,
     forecastData: `It is ${forecastData.current.weather_descriptions[0]} outside with ${forecastData.current.temperature} degrees and ${forecastData.current.precip}% chance of rain.`,
